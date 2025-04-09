b88a69b83765b998cd74749a3316d54450c2640d
Zhenmiao Zhang <zhz142@ucsd.edu>
2025-04-09 11:00:26 -0700

    latest

--- src/scripts/remove_chimeric.sh
+++ src/scripts/remove_chimeric.sh
@@ -3,6 +3,6 @@ asm=$2
 outprefix=$3
 
 # compress --dimer-compress 32,32,1 --reads $1 > $outprefix.compressed.fasta
-/Poppy/zmzhang/software/anaconda3/envs/clja/bin/minimap2 -ax map-hifi $asm $outprefix.compressed.fasta -t 100 | samtools sort -@ 100 -o $outprefix.bam
+# /Poppy/zmzhang/software/anaconda3/envs/clja/bin/minimap2 -ax map-hifi $asm $outprefix.compressed.fasta -t 100 | samtools sort -@ 100 -o $outprefix.bam
 
-python /Poppy/zmzhang/Consensus_Assembly/src/analyze_chimeric.py $outprefix.bam  $reads $outprefix.filtered.fastq
+python /Poppy/zmzhang/Consensus_Assembly/src/analyze_chimeric.py $outprefix.bam $reads $outprefix.filtered.fastq
