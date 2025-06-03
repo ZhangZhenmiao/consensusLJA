478c319b317ba6c5a2d3d763fdae2c3de2443a69
Zhenmiao Zhang <zhz142@ucsd.edu>
2025-06-02 22:47:20 -0700

    latest

--- src/dbg/run_dbg.cpp
+++ src/dbg/run_dbg.cpp
@@ -23,14 +23,30 @@ void DBGRunner::cleanDBG() {
     //     std::cout << "Removed " << removed_edges << " low-coverage edges" << std::endl;
     // }
     removed_edges = 1;
+    // 10 for hist figure, auto for results
     while (removed_edges) {
-        graph.remove_low_coverage_edges(removed_edges, graph.first_minima, true);
+        graph.remove_low_coverage_edges(removed_edges, 10, true);
         std::cout << "Removed " << removed_edges << " low-coverage tips" << std::endl;
     }
     this->first_peak = graph.error_peak;
     this->first_minima = graph.first_minima;
     graph.write_graph(output + "/graph.remove_low");
     graph.write_graph_gfa(output + "/graph.remove_low");
+
+    std::unordered_set<std::string> nodes_linked_to_low;
+    for (auto&& node : graph.graph) {
+        for (auto&& n_out : node.second.outgoing_edges) {
+            for (auto&& e : n_out.second) {
+                if (e.multiplicity <= 10) {
+                    nodes_linked_to_low.insert(node.first);
+                    nodes_linked_to_low.insert(n_out.first);
+                }
+            }
+        }
+    }
+
+    graph.get_annotation(output + "/graph.remove_low");
+    graph.write_graph(output + "/graph.remove_low_only", 1000000, false, true, nodes_linked_to_low);
 
     // std::cout << "----------Stage 2: Remove chimeric edges ----------" << std::endl;
     // graph.detect_chimeric_reads();
